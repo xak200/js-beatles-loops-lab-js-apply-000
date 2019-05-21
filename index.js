a7e61c410bee8e5aa66f92bf770793db6085acaa
Xaria Kirtikar <xariak91@gmail.com>
2019-05-21 15:54:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function theBeatlesPlay(musicians, instruments) {
   // for (let i = 0; i < musicians.length; i++) {
   //   statements.push(`${musicians[i]} plays ${instruments[i]}`);
   // }
-  
+  musicians.forEach()
   return statements;
 }
 
