1dbcb45919233f8d9c35e6b347226686bdf15b99
Xaria Kirtikar <xariak91@gmail.com>
2019-05-21 16:25:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,19 +28,11 @@ points.*/
 function johnLennonFacts(factsArr) {
   let counter = 0;
   while (counter < factsArr.length) {
-    factsArr[counter] = factsArr[counter] + "!!!";
+    factsArr[counter] += "!!!";
     counter++;
   }
   return factsArr;
 }
-
-/* + Create a function `iLoveTheBeatles` which accepts a number as a parameter. The
-body of the function should create a variable that stores an empty array. Then,
-implement a do-while loop inside the function that adds `"I love the Beatles!"`
-to the empty array. Then the loop should increment the number passed in as a
-parameter. The condition of the loop should check to see that the parameter
-number is less than `15`. The function should return the array with the strings
-`"I love the Beatles!"`. */
 
 function iLoveTheBeatles(number) {
   const love = [];
